324444f570ec8eeecfd10003c8db44ef3e345aff
Jason Ward <jason55ward@gmail.com>
2022-08-26 14:24:54 +0200

    data loading complete

--- load_data.py
+++ load_data.py
@@ -1,44 +1,58 @@
 import os
 import glob
 from constants import *
+import zipfile
+from dateutil import parser
 
 
-def load_data(date_time, minutes):
-    """
-    Reads the data from the files
-    """
-    def get_filename(file_list, file_search):
-        for filename in file_list:
-            if file_search in filename.lower():
-                return filename
-
-    def first_substring(the_list, substring):
-        return [idx for idx, s in enumerate(the_list) if substring in s][0]
-
+def load_ticks(date_time):
+    date_time = parser.parse(date_time)
     currency_dir = os.path.join(DATA_DIR, CURRENCY_PAIR)
-    path = os.path.join('.', currency_dir, "*")
-    filenames = glob.glob(path)
-    one_minute_index = first_substring(filenames, 'bid_1_m_2020-2022')
-    five_minute_index = first_substring(filenames, 'bid_5_m_2020-2022')
-    fifteen_minute_index = first_substring(filenames, 'bid_15_m_2020-2022')
-    one_hour_index = first_substring(filenames, 'bid_1_hour_2020-2022')
-    four_hour_index = first_substring(filenames, 'bid_4_hour_2020-2022')
-    daily_index = first_substring(filenames, 'bid_1_d_2020-2022')
+    tick_dir = os.path.join(currency_dir, TICK_DIR_NAME)
+    tick_zip_files = glob.glob(os.path.join('.', tick_dir, "*"))
+    tick_zip_filename = f"{CURRENCY_PAIR}_T_BID{date_time.year}{str(date_time.month).rjust(2,'0')}.zip"
+    tick_zip_file_path = os.path.join(tick_dir, tick_zip_filename)
+    if not os.path.exists(tick_zip_file_path):
+        raise FileNotFoundError((f"Cant't find file {tick_zip_file_path}"))
 
-    with open(filenames[one_minute_index]) as bid_file:
-        self.one_minute_data = tuple(bid_file.readlines())
-    bid = self.one_minute_data
-    ask = self.bid
+    tick_file = f"DAT_NT_{CURRENCY_PAIR}_T_BID_{date_time.year}{str(date_time.month).rjust(2,'0')}.csv"
+    
+    ticks = []
+    with zipfile.ZipFile(tick_zip_file_path) as thezip:
+        with thezip.open(tick_file, mode='r') as thefile:
+            for line in thefile:
+                ticks.append(line.decode("utf-8"))
 
-    with open(filenames[five_minute_index]) as bid_file:
-        five_minute_data = tuple(bid_file.readlines())
-    with open(filenames[fifteen_minute_index]) as bid_file:
-        fifteen_minute_data = tuple(bid_file.readlines())
-    with open(filenames[one_hour_index]) as bid_file:
-        one_hour_data = tuple(bid_file.readlines())
-    with open(filenames[four_hour_index]) as bid_file:
-        four_hour_data = tuple(bid_file.readlines())
-    with open(filenames[daily_index]) as bid_file:
-        daily_data = tuple(bid_file.readlines())
+    return ticks
 
-    return [one_minute_data, five_minute_data, self.fifteen_minute_data, self.one_hour_data, self.four_hour_data, self.daily_data]
+def load_minutes(date_time):
+    date_time = parser.parse(date_time)
+    currency_dir = os.path.join(DATA_DIR, CURRENCY_PAIR)
+    one_minute_dir = os.path.join(currency_dir, ONE_MINUTE_DIR_NAME)
+    one_minute_zip_files = glob.glob(os.path.join('.', one_minute_dir, "*"))
+
+    one_min_zip_filename = f"{CURRENCY_PAIR}_M1{date_time.year}.zip"
+    one_min_zip_file_path = os.path.join(one_minute_dir, one_min_zip_filename)
+    if not os.path.exists(one_min_zip_file_path):
+        raise FileNotFoundError((f"Cant't find file {one_min_zip_file_path}"))
+    prior_one_min_zip_filename = f"{CURRENCY_PAIR}_M1{date_time.year-1}.zip"
+    prior_one_min_zip_file_path = os.path.join(one_minute_dir, prior_one_min_zip_filename)
+    if date_time.year != OLDEST_DATA_YEAR and not os.path.exists(prior_one_min_zip_file_path):
+        raise FileNotFoundError((f"Cant't find file {prior_one_min_zip_file_path}"))
+
+    prior_one_min_file = f"DAT_NT_{CURRENCY_PAIR}_M1_{date_time.year-1}.csv"
+    one_min_file = f"DAT_NT_{CURRENCY_PAIR}_M1_{date_time.year}.csv"
+    
+    minutes = []
+    if date_time.year != OLDEST_DATA_YEAR:
+        with zipfile.ZipFile(prior_one_min_zip_file_path) as thezip:
+            with thezip.open(prior_one_min_file, mode='r') as thefile:
+                for line in thefile:
+                    minutes.append(line.decode("utf-8"))
+
+    with zipfile.ZipFile(one_min_zip_file_path) as thezip:
+        with thezip.open(one_min_file, mode='r') as thefile:
+            for line in thefile:
+                minutes.append(line.decode("utf-8"))
+
+    return minutes
