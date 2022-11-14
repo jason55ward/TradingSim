9f9a632e0d9eefa6760834d50dcc48ab3caa58c4
jason <jason55ward@gmail.com>
2022-11-14 07:33:53 +0200

    adjusted speed

--- events.py
+++ events.py
@@ -73,9 +73,9 @@ class Events():
                 if event.key == pygame.K_KP_MINUS:
                     self.settings.candle_width -= 1
                 if event.key == pygame.K_EQUALS:
-                    self.settings.time_speed += 20
+                    self.settings.time_speed += 10
                 if event.key == pygame.K_MINUS:
-                    self.settings.time_speed -= 20
+                    self.settings.time_speed -= 10
                 if event.key == pygame.K_PAUSE:
                     self.state.paused = not self.state.paused
             if event.type == pygame.MOUSEMOTION and pygame.mouse.get_pressed()[0]:
